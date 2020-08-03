e642347c9c3fa5635246432209b3fef688a3d90c
mixxamm <maximjanssensj5@gmail.com>
2020-08-03 21:14:58 +0200

    Nvm vorige commit
    Werkte dus wel gewoon, vue debugger deed wat lastig.

--- pages/index.vue
+++ pages/index.vue
@@ -16,18 +16,20 @@
 const signalR = require('@microsoft/signalr')
 const url = 'https://localhost:44372/smshub'
 
-const connection = new signalR.HubConnectionBuilder().withUrl(url).build()
-connection.start().then(() => {
-  console.log('Verbinding gestart')
-}).catch(err => console.error('Verbinding mislukt: ' + err))
-connection.on('ReceiveSMS', (sms) => {
-  console.log(sms)
-})
 export default {
   data () {
     return {
       smsen: []
     }
+  },
+  created () {
+    const connection = new signalR.HubConnectionBuilder().withUrl(url).build()
+    connection.start().then(() => {
+      console.log('Verbinding gestart')
+    }).catch(err => console.error('Verbinding mislukt: ' + err))
+    connection.on('ReceiveSMS', (sms) => {
+      this.smsen.push(sms)
+    })
   }
 }
 </script>
