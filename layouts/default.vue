409452c2134439c9328dff51c2f364be4c024c93
mixxamm <maximjanssensj5@gmail.com>
2021-09-03 15:38:57 +0200

    Add clock

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,6 +13,7 @@
         style="text-decoration: none;
     color: white!important;"
       ><v-icon class="mr-2">mdi-github</v-icon><span>mixxamm</span></a>
+      <v-spacer></v-spacer><div @click="hideClock = !hideClock" :style="{opacity: hideClock ? '0%' : '100%', cursor: 'pointer'}">{{hours}}:{{minutes}}:{{seconds}}</div>
     </v-footer>
   </v-app>
 </template>
@@ -21,7 +22,27 @@
 export default {
   data () {
     return {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      hideClock: false
     }
+  },
+  methods: {
+    setTime () {
+      setInterval(() => {
+        const date = new Date()
+        this.hours = date.getHours()
+        this.minutes = this.checkSingleDigit(date.getMinutes())
+        this.seconds = this.checkSingleDigit(date.getSeconds())
+      }, 1000)
+    },
+    checkSingleDigit (digit) {
+      return ('0' + digit).slice(-2)
+    }
+  },
+  created () {
+    this.setTime()
   }
 }
 </script>
